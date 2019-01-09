69f94673449e47b2a689f67ef250bf2c92953e04
Amit Bothra <amit.bothra@outlook.com>
2019-01-09 06:56:35 -0600

    created diagnose fn from dlookr package for learning

--- code/rscripts/Step01_EDA_Dplyr.R
+++ code/rscripts/Step01_EDA_Dplyr.R
@@ -1,6 +1,31 @@
-library(dplyr);
+# We will try to replicate functions of dlookr package for learning purpose
 
+library(dplyr)
+library(tidyr)
 #load Data
 flight_2013 <- readRDS("data/produced_data/flight_2013.Rds")
+x 
+# Create dlookr function diagnose () 
+# 
+# variables : variable name
+# types : the data type of the variable
+# missing_count : number of missing values
+# missing_percent : percentage of missing values
+# unique_count : number of unique values
+# unique_rate : rate of unique value. unique_count / number of observation
 
-head(flight_2013)
+diagnose <- flight_2013 %>%
+            summarise_all(funs(
+              types = class(.)[1],
+              missingcount = sum(as.numeric(is.na(.))),
+              missingpercent = round(mean(as.numeric(is.na(.)))*100,digits=1),
+              uniquecount = n_distinct(.),
+              uniquerate = round((n_distinct(.)/n())*100,digits=1)
+            )) %>%
+            gather(variable,value) %>%
+            separate(variable,c("var","stat"), sep="_(?=[^_]*$)") %>%
+            spread(stat,value) %>%
+            rename(variables = var) %>%
+            select(variables,types,missingcount,missingpercent,uniquecount,uniquerate) %>%
+            as.data.frame()
+            
